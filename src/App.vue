beb1ea44c3fbb45d0971be823614c73d048ac83f
Nicole Driebe <blongsch@nicoles-MBP-2.fritz.box>
2024-07-03 17:03:11 +0200

    first step

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+   
+  </header>
+
+  <main>
+    <h1>Hello</h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
